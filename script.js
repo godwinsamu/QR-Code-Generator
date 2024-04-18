0e3fed1eb3a43e0d4a41ee7f4b9fd5f37ae6139e
godwinsamu <164744237+godwinsamu@users.noreply.github.com>
2024-04-18 10:56:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let qrContentInput = document.getElementById("qr-content");
+let qrForm = document.getElementById("qr-form");
+let qrCode;
+function generateQrCode(qrContent){
+return new QRCode("qr-code",{
+    text : qrContent,
+    width:256,
+    height:256,
+    colorDark :"#000000",
+    colorLight:"#ffffff",
+    correctLevel : QRCode.CorrectLevel.H,
+});
+} 
+qrForm.addEventListener('submit',function(event){
+    event.preventDefault();
+    let qrContent = qrContentInput.value;
+if(qrCode==null){
+    qrCode = generateQrCode(qrContent);
+}
+else{
+    qrCode.makeCode(qrContent);
+}
+});
